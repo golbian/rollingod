bff6a248478b65838dfe3007765c1e6d938ac8e1
Augustin ANTOINE <augustin.antoine.pro@gmail.com>
2020-12-17 16:04:43 +0100

    test tab diceCreation

---
+++ src/app/dice-display/dice-display.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dice-display',
+  templateUrl: './dice-display.component.html',
+  styleUrls: ['./dice-display.component.scss'],
+})
+export class DiceDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
