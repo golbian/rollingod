f476a7fa8be50a7b34a311469ae6f2b94b94449c
alexandremessang <alexandre.messang@gmail.com>
2020-12-17 14:37:44 +0100

    add zeus

---
+++ src/app/zeus/zeus.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zeus',
+  templateUrl: './zeus.component.html',
+  styleUrls: ['./zeus.component.scss'],
+})
+export class ZeusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
