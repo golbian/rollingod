414ef2b7fc3280a23a12b32b37984e4e773416dd
alexandremessang <alexandre.messang@gmail.com>
2021-01-21 10:55:50 +0100

    add opaque image

--- src/app/zeus/zeus.component.ts
+++ src/app/zeus/zeus.component.ts
@@ -12,37 +12,28 @@ export class ZeusComponent implements OnInit {
     name : "Zeus",
     description : "Lance un terrible éclair directement sur l'adversaire",
     powers : [
-      "4 faveurs inflige 2 dégats à l'adversaire",
-      "8 faveurs inflige 5 dégats à l'adversaire",
-      "12 faveurs inflige 8 dégats à l'adversaire",
+      {
+        favor: 4,
+        damage: 2,
+        description: "4 faveurs inflige 2 dégats à l'adversaire",
+      },
+      {
+        favor: 8,
+        damage: 5,
+        description: "8 faveurs inflige 5 dégats à l'adversaire",
+      },
+      {
+        favor: 12,
+        damage: 8,
+        description: "12 faveurs inflige 8 dégats à l'adversaire",
+      },
+
     ]
   }
 
   ////////////////////////////////// FONCTIONS //////////////////////////////////
 
-  /**
-   * Retourne la valeur de dégât du pouvoir selectionné
-   * 
-   * @param element item sélectionné sur l'interface
-   */
-  public damage(element:string) {
-    let selection = this.zeus.powers.indexOf(element);
-    console.log(selection);
-    switch (selection) {
-      case 0: {
-          return 2;
-      }
-      case 1: {
-        return 5;
-      }
-      case 2: {
-        return 8;
-      }
-      default: {
-        return -1;
-      }
-    } 
-  }
+
 
 
 
